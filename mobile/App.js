6ddb252d1d691fcacfa58b1dc564502a2e0cb350
Jefferson Luiz <jeffersonluiz.dev@gmail.com>
2020-01-23 14:39:54 -0800

    botoes do menu adicionados

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,69 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import * as Animatable from 'react-native-animatable';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>LIGA DOS SURDOS IS COMMING</Text>
+      <Image style={styles.startscreen}
+        source = {require("./assets/EO-TFu2WsAE7Hnm.png")}  
+      />
+      
+      <Animatable.View
+            animation="fadeInLeft"
+            useNativeDriver
+            duration={2000}
+            style={styles.initialbutton}
+          >
+          <TouchableOpacity >
+            <Text>Entrar</Text>
+          </TouchableOpacity>
+      </Animatable.View>
+
+        <Animatable.Text style={styles.register}
+          animation="fadeInRight"
+          useNativeDriver
+          duration={2000}
+        >
+          Não tem uma conta? Clique aqui.
+          </Animatable.Text>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  startscreen: {
+    width: 370,
+    height: 660,
+    marginLeft: -10,
+    marginTop: -20
+  },
+
+  initialbutton: {
+    position: "absolute",
+    width: '82%',
+    height: 35,
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 560,
+    marginLeft: 29
+  },
+
+  register: {
+    position: "absolute",
+    width: '60%',
+    height: 35,
+    color: '#fff',
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 605,
+    marginLeft: 73
+  }
+
 });
